056c966c70e19754ab364f79f2c6f3ce24bf655a
EmmanuelArowoselu <arowoseluemmanuel@gmail.com>
2023-04-15 00:08:05 -0500

    🔥

--- sketch.js
+++ sketch.js
@@ -133,23 +133,3 @@ class Particle {
     ellipse(this.pos.x, this.pos.y, this.w)
   }
 }
-
-
-//function touchStarted () {
-//  var fs = fullscreen();
-//  if (!fs) {
-//    fullscreen(true);
-//  }
-//}
-
-/* full screening will change the size of the canvas */
-//function windowResized() {
-//  resizeCanvas(windowWidth, windowHeight);
-//}
-
-/* prevents the mobile browser from processing some default
- * touch events, like swiping left for "back" or scrolling the page.
- */
-//document.ontouchmove = function(event) {
-//    event.preventDefault();
-//};
